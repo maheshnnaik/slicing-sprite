214faa4a54e5e11bb9605efad2af91993b692a6e
maheshnnaik <maheshnaik2747@gmail.com>
2022-09-27 19:31:15 +0530

    Add fonts and token list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { SwapWidget } from '@uniswap/widgets';
+import TOKEN_LIST from './Token_List.json'
 import './App.css';
-import '@uniswap/widgets/fonts.css'
+import './fonts.css';
+
+const DMD = "0x6702D328CDc0cC438AB5C667E7A2033f8065C64F"
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <SwapWidget />
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className = 'uniswap-widget'>
+				<SwapWidget
+					tokenList={TOKEN_LIST}
+					defaultOutputTokenAddress={DMD}
+					defaultOutputAmount={100}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
