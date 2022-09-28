d0160c48b221ef8a4cc827cc2e11fe1361568dcd
maheshnnaik <maheshnaik2747@gmail.com>
2022-09-28 11:15:42 +0530

    read input amount from url parameter

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,29 @@ import TOKEN_LIST from './Token_List.json'
 import './App.css';
 import './fonts.css';
 
-const DMD = "0x6702D328CDc0cC438AB5C667E7A2033f8065C64F"
+const TOKENS = {
+	"DMD": "0x6702D328CDc0cC438AB5C667E7A2033f8065C64F",
+	"WMATIC": "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889",
+	"WETH": "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa",
+	"NATIVE": "NATIVE"
+}
 
+const windowUrl = window.location.search;
+const params = new URLSearchParams(windowUrl);
+const isSwap = params.get('isSwap');
+const defaultInputAmount: any = params.get('defaultInput');
+
+const defaultInputToken = (isSwap === "true") ? TOKENS.NATIVE : TOKENS.DMD;
+const defaultOutputToken = (isSwap === "true") ? TOKENS.DMD : TOKENS.NATIVE;
 function App() {
 	return (
 		<div className="App">
 			<div className = 'uniswap-widget'>
 				<SwapWidget
 					tokenList={TOKEN_LIST}
-					defaultOutputTokenAddress={DMD}
-					defaultOutputAmount={100}
+					defaultInputTokenAddress={defaultInputToken}
+					defaultOutputTokenAddress={defaultOutputToken}
+					defaultInputAmount={defaultInputAmount}
 				/>
 			</div>
 		</div>
